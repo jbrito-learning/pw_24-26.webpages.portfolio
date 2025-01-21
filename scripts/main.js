674446da0355782ed6d3e290fb9b57160782bb44
jbrito <joaodcbrito7@gmail.com>
2025-01-21 19:36:33 +0000

    feat: implement dark mode toggle functionality and update HTML structure
    - Added a dark mode toggle button in the HTML with corresponding event handler in main.js.
    - Implemented dark mode styles in global.css, including visibility toggles for sun and moon icons.
    - Updated the main title to have an ID for potential future JavaScript interactions.
    - Removed the avatar image for a cleaner design.

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,24 @@ async function loadFooter() {
 
 loadFooter();
 loadNavbar();
+
+function checkDarkMode() {
+  const hasDarkMode = window.localStorage.getItem("darkMode");
+  if (hasDarkMode) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+}
+
+checkDarkMode();
+
+function toggleDarkMode() {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+    window.localStorage.removeItem("darkMode");
+  } else {
+    document.body.classList.add("dark");
+    window.localStorage.setItem("darkMode", "true");
+  }
+}
